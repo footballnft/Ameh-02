f95b71848fef9c6b302bb9ccc82b021da452d568
footballnft <85647902+footballnft@users.noreply.github.com>
2022-04-17 14:36:14 +0100

    integrated sentry.io

--- next.config.js
+++ next.config.js
@@ -1,7 +1,32 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const { withSentryConfig } = require('@sentry/nextjs')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
+
+const sentryWebpackPluginOptions =
+  process.env.VERCEL_ENV === 'production'
+    ? {
+        // Additional config options for the Sentry Webpack plugin. Keep in mind that
+        // the following options are set automatically, and overriding them is not
+        // recommended:
+        //   release, url, org, project, authToken, configFile, stripPrefix,
+        //   urlPrefix, include, ignore
+        silent: false, // Logging when deploying to check if there is any problem
+        validate: true,
+        release: process.env.VERCEL_GIT_COMMIT_SHA,
+        // Set to env false will skip deploying release on Sentry except Production
+        // https://github.com/getsentry/sentry-webpack-plugin/blob/master/src/index.js#L522
+        deploy: {
+          env: process.env.VERCEL_ENV,
+        },
+        // For all available options, see:
+        // https://github.com/getsentry/sentry-webpack-plugin#options.
+      }
+    : {
+        silent: true, // Suppresses all logs
+        dryRun: !process.env.SENTRY_AUTH_TOKEN,
+      }
 
 /** @type {import('next').NextConfig} */
 const config = {
@@ -77,4 +102,4 @@ const config = {
   },
 }
 
-module.exports = withBundleAnalyzer(config)
+module.exports = withBundleAnalyzer(withSentryConfig(config, sentryWebpackPluginOptions))
